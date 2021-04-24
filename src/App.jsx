3b3e48c12c549ef30d899966d8886fcadc28458b
Sam Bui <sam@Sams-Air.hub>
2021-04-24 14:38:34 +1000

    react d3 barchart basic implementation

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./styles/App.css";
+import { scaleLinear, scaleBand, extent } from "d3";
+
+import LoadData from "./LoadData";
+
+const App = () => {
+  const dataArrayObjs = LoadData();
+
+  const margin = { top: 20, right: 30, bottom: 70, left: 220 };
+  const widthSVG = 960;
+  const heightSVG = 500;
+  const innerWidth = widthSVG - margin.right - margin.left;
+  const innerHeight = heightSVG - margin.top - margin.bottom;
+
+  const xValue = (aDataObj) => aDataObj["2020NumType"];
+  const minNmax2020PopArray = extent(dataArrayObjs.map(xValue));
+
+  // xScale to adjust the values in the x axis
+  // in the case of horizontal bar chart, x axis is the actual value of each categories
+
+  const xScale = scaleLinear()
+    .domain(minNmax2020PopArray)
+    .range([0, innerWidth]);
+
+  const yValue = (aDataObj) => aDataObj["Country"];
+  // collect the country names into an array
+  const countryNamesArray = dataArrayObjs.map(yValue);
+
+  // yScale to adjust the values in the y axis, in this case, Countries
+  let yScale = scaleBand()
+    .domain(countryNamesArray)
+    .range([0, innerHeight])
+    .paddingInner(0.15);
+
+  console.log(minNmax2020PopArray);
+
+  return (
+    <>
+      <h1>This is a bar chart</h1>
+      <svg width={widthSVG} height={heightSVG}>
+        <g transform={`translate(${margin.left}, ${margin.top})`}>
+          {dataArrayObjs.map((aDataObj, index) => (
+            <rect
+              key={index}
+              x={0}
+              y={yScale(yValue(aDataObj))}
+              width={xScale(xValue(aDataObj))}
+              height={yScale.bandwidth()}
+            />
+          ))}
+        </g>
+      </svg>
+    </>
+  );
+};
+
+export default App;
